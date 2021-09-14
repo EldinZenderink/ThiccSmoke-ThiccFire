b90a5f610ea1e84f64317ef8b39ba430c316992e
EldinZenderink <e.zenderink@gmail.com>
2021-09-14 19:58:46 +0200

    Fix issue with version checker not triggering restoring default values
    + Some additionaly removal of unused code :P.

--- options.lua
+++ options.lua
@@ -10,25 +10,27 @@
 #include "firedetector.lua"
 
 function init()
-   Debug_ClearDebugPrinter()
-   -- Determine version and if maybe the previous stored data should be transferred
-   local version_state = Version_Init("ThiccSmoke")
-   local set_default = false
-   if version_state == "store_default" or version_state == "transfer_stored" then
-       set_default = true
-   end
-
-   Storage_Init(Version_GetName(), Version_GetCurrent())
-   GeneralOptions_Init(set_default)
-   Debug_Init()
-   FireDetector_Init(set_default)
-   Particle_Init(set_default)
-   Material_Init(set_default)
-   Menu_Init(set_default)
-   Menu_AppendMenu(GeneralOptions_GetOptionsMenu())
-   Menu_AppendMenu(FireDetector_GetOptionsMenu())
-   Menu_AppendMenu(Particle_GetOptionsMenu())
-   Menu_AppendMenu(Material_GetOptionsMenu())
+    Debug_ClearDebugPrinter()
+    -- Determine version and if maybe the previous stored data should be transferred
+    local version_state = Version_Init("ThiccSmoke")
+    local set_default = false
+    if version_state == "store_default" or version_state == "transfer_stored" then
+        set_default = true
+    end
+    Storage_Init(Version_GetName(), Version_GetCurrent())
+    GeneralOptions_Init(set_default)
+    Debug_Init()
+    FireDetector_Init(set_default)
+    ParticleSpawner_Init(set_default)
+    Particle_Init(set_default)
+    Material_Init(set_default)
+    Menu_Init(set_default)
+    Menu_AppendMenu(GeneralOptions_GetOptionsMenu())
+    Menu_AppendMenu(FireDetector_GetOptionsMenu())
+    Menu_AppendMenu(ParticleSpawner_GetOptionsMenu())
+    Menu_AppendMenu(Particle_GetOptionsMenu())
+    Menu_AppendMenu(Material_GetOptionsMenu())
+    DebugPrinter("version state: " .. version_state)
 end
 
 function draw()
